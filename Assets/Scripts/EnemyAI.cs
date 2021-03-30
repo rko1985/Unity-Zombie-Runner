cb680721ec70c488507d0e36ca09f4453480d263
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-03-30 09:33:51 -1000

    enemy chase if within range

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,7 +7,10 @@ public class EnemyAI : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,10 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
     }
 }
